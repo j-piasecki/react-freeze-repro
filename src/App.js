94bf92132aac6fd4ae879de9f2a09bf1b5a619ce
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-07-28 13:58:26 +0200

    aaa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import {Freeze} from 'react-freeze';
 
-function App() {
+const listeners = [];
+
+function addListener(listener) {
+  listeners.push(listener);
+
+  return () => {
+    const index = listeners.indexOf(listener);
+    listeners.splice(index, 1);
+  };
+}
+
+let test = true;
+
+function ComponentWithLayout() {
+  const [a, setA] = React.useState(0);
+
+  React.useEffect(() => {
+    console.log('Test Mount');
+
+    return () => {
+      console.log('Test Unmount');
+    };
+  }, []);
+  console.log('Test render');
+
+  if (test) {
+    queueMicrotask(() => {
+      console.log('Test sdfasdfasdf');
+      setA(1);
+    })
+    test = false;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{width: 100, height: 100, backgroundColor: 'blue'}}
+    >
+      <span>{a}</span>
     </div>
   );
 }
 
-export default App;
+function DelayedRender(props) {
+  const [visible, setVisible] = React.useState(false);
+
+  React.useEffect(() => {
+    console.log('DelayedRender immediate');
+    setVisible(true);
+
+    listeners.forEach(listener => listener());
+  }, []);
+
+  if (visible) {
+    console.log('DelayedRender done');
+    return props.children;
+  } else {
+    console.log('DelayedRender waiting');
+    return null;
+  }
+}
+
+function FreezeComponent({frozen}) {
+  return (
+    <div style={{width: 100, height: 100}}>
+      <Freeze freeze={frozen}>
+        <div style={{width: 100, height: 100, backgroundColor: 'red'}} />
+      </Freeze>
+    </div>
+  );
+}
+
+function DelayedComponent({visible}) {
+  return (
+    <div style={{width: 100, height: 100}}>
+      {visible && (
+        <DelayedRender>
+          <ComponentWithLayout />
+        </DelayedRender>
+      )}
+    </div>
+  );
+}
+
+function FreezeTest() {
+  const [frozen, setFrozen] = React.useState(false);
+  const [visible, setVisible] = React.useState(false);
+
+  useEffect(() => {
+    const unsubscribe = addListener(() => {
+      setFrozen(!frozen);
+      console.log('freezed:', !frozen);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [frozen]);
+
+  return (
+    <div style={{flex: 1}}>
+      <div style={{height: 100, flexDirection: 'row'}}>
+        <FreezeComponent frozen={frozen} />
+        <DelayedComponent visible={visible} />
+      </div>
+
+      <input
+          type="button"
+          value="Do the thing"
+          onClick={() => {
+            setVisible(!visible);
+
+            if (visible) {
+              setFrozen(false);
+            }
+          }}
+        />
+    </div>
+  );
+}
+
+export default FreezeTest;
